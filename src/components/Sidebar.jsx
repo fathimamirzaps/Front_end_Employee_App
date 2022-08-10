4d387d9652374f0a485393929351523bd0394845
fathimamirzaps <fathimamirzaps@gmail.com>
2022-08-10 23:45:49 +0530

    added logout button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,15 @@
 import '../style/style.css';
 import Logo from '../assets/kv logo.png';
+import { useNavigate } from 'react-router-dom';
+import {removeStorage} from "../services/utils"
+import Button from "./Button";
 import list from'../assets/List.png';
 const SideBar = ({label,options,placeholder,onChange}) => {
+    const navigate=useNavigate();
+    const handlelogout =() =>{
+        removeStorage("idToken");
+        navigate("/");  
+    };
     return(
         <aside>
             <header><img src = {Logo} alt ="kv logo" id = "kvlogo"/>
@@ -17,6 +25,7 @@ const SideBar = ({label,options,placeholder,onChange}) => {
                     
                 </div>
                 
+                <Button id="logout" label="Logout" onClick={()=>handlelogout()}/>
 
             </nav>
         </header>
